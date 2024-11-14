0c8a43419b812f5137310449b37677a5c600b23a
roman <skyfoxwork@gmail.com>
2024-11-14 19:08:57 +0200

    Solution-py-taxi-service-startproject v1.0

--- taxi/admin.py
+++ taxi/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin
 
 from taxi.models import Manufacturer, Driver, Car
 
+
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
     list_display = UserAdmin.list_display + ("license_number", )
@@ -12,6 +13,7 @@ class DriverAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         ("Additional info", {"fields": ("license_number", )}),
     )
+
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
